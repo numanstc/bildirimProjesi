03570ff6864bd27f8681ce20ce48c333b49cd499
Numan SATICI <numan_satici@hotmail.com>
2020-06-19 20:40:29 +0300

    uuid çıkarıldı

--- src/sqlite/insert.js
+++ src/sqlite/insert.js
@@ -1,12 +1,8 @@
 import {openDatabase} from 'react-native-sqlite-storage';
-var uuid = require('react-native-uuid');
 
 const db = openDatabase({name: 'duyuru1.db', location: 'default'});
 
 export function insertPageLinks(pages, toplamVeri) {
-  let id = uuid.v4();
-  console.log(id);
-
   let sira = toplamVeri;
   db.transaction(
     (tx) => {
