cee47bc6ffb90cefd3ff923ac9d40533994166f0
Numan SATICI <numan_satici@hotmail.com>
2020-06-20 21:34:42 +0300

    duyuru kontrol Tamamlandı

--- src/componenets/denemeComp.js
+++ src/componenets/denemeComp.js
@@ -14,7 +14,6 @@ export default function DenemeComp() {
     ek: [],
   });
 
-  const [load, setLoad] = useState(false);
   const [insertPageBool, setInsertPageBool] = useState(true);
   const deger = [
     {
@@ -34,35 +33,17 @@ export default function DenemeComp() {
     },
   ];
 
-  // const pageDeger = [
-  //   {
-  //     text:
-  //       ' Aşağıdaki öğrencilerin Proje 2 dersini hangi hocadan alacaklar ise örnek dilekçeyi doldurup imzalı olarak bölüm sekreterliğine en geç 28 Şubat Cuma günü mesai bitimine kadar ulaştırması gerekmektedir. Öğrenci listesi : 060401024 Sedat Şanan  060401031 Bahattin Carav  090401005 Selman Şahiner  100401048 Oğuz Kılıç  110401049 Adem Altungeyik  130401042 Bahar ÇİFTÇİ  130401055 Nurşah Koç  130401074 Augusto Gomes Junior  150401055 Numan SATICI  150401015 Enes Bal   150401059 İsmail Oğuzcan ARSLAN  150401069 Mustafa Burak Kılıç',
-  //     link: [
-  //       {
-  //         name: 'Dilekçe örneği',
-  //         link:
-  //           'https://cdn.comu.edu.tr/cms/muhendislik.ce/files/132-proje-ii-dilekcesi.pdf',
-  //       },
-  //       {
-  //         name: 'View this pdf file',
-  //         link:
-  //           'https://docs.google.com/viewer?url=https%3A%2F%2Fcdn.comu.edu.tr%2Fcms%2Fmuhendislik.ce%2Ffiles%2F132-proje-ii-dilekcesi.pdf&embedded=true&chrome=false&dov=1',
-  //       },
-  //     ],
-  //     image: [],
-  //     ek: [],
-  //   },
-  // ];
-
   useEffect(() => {
-    insertPageLinks(deger, 10);
+    // insertPageLinks(deger, 10);
 
     setPageLink(selectPageLinks());
-    setLoad(true);
   }, []);
 
-  // const kontrolEt = useDuyuruKontrol();
+  const [kontrolEt, loading] = useDuyuruKontrol();
+  if (!loading) {
+    console.log('Hook çağırıldı deger:' + kontrolEt);
+    console.log('Hook çağırıldı load:' + loading);
+  }
 
   // useEffect(() => {
   //   if (insertPageBool) {
--- src/hooks/useDuyuruKontrol.js
+++ src/hooks/useDuyuruKontrol.js
@@ -4,26 +4,41 @@ import {selectPageLinks} from '../sqlite/select';
 import duyuruKontrol from '../webScraping/duyuruKontrol';
 
 function useDuyuruKontrol() {
-  const [kontrolEt, setKontrolEt] = useState(false);
-  const [kayitliSonLink, setKayitliSonLink] = useState([{sira: 1}]);
+  const [kontrolEt, setKontrolEt] = useState(null);
+  const [pageLink, setPageLink] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [uzunluk, setUzunluk] = useState({
     p: 1,
     lenght: 1,
   });
-
+  const getdata = async () => {
+    const tut = await duyuruKontrol();
+    setUzunluk({
+      p: tut.p,
+      lenght: tut.lenght,
+    });
+    setLoading(false);
+  };
   useEffect(() => {
-    setUzunluk(duyuruKontrol());
-    setKayitliSonLink(selectPageLinks());
+    getdata();
+    setPageLink(selectPageLinks(1));
   }, []);
 
   useEffect(() => {
-    if (kayitliSonLink[0].sira !== (uzunluk.p - 1) * 15 + uzunluk.lenght)
-      setKontrolEt(true);
-    console.log(
-      'Hook Çalıştı Uzunluk: ' + (uzunluk.p - 1) * 15 + uzunluk.lenght,
-    );
-  }, []);
-  return kontrolEt;
+    if (!loading) {
+      if ((uzunluk.p - 1) * 15 + uzunluk.lenght === pageLink[0].sira) {
+        setKontrolEt(false);
+      } else {
+        setKontrolEt(true);
+      }
+      console.log(
+        'Uzunluk.p: ' + uzunluk.p + ' Uzunluk.lenght: ' + uzunluk.lenght,
+      );
+
+      console.log('sonLink: ' + pageLink[0].sira);
+    }
+  }, [loading]);
+  return [kontrolEt, loading];
 }
 
 export default useDuyuruKontrol;
