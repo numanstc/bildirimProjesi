cd77baed5b9791bda3f2f4ceb9d41477efff7ee7
Numan SATICI <numan_satici@hotmail.com>
2020-06-29 22:22:45 +0300

    gereksiz yorum satırları kaldırıldı

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -2,32 +2,12 @@ import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-// import Tags from 'kodilan../sqlIte2/createTables/Tags';
-// import timeSince from 'k../sqlIte2/insertPagespers/timeSince';
-// import PostType fro../sqlIte2/selecte/src/components/PostType';
-// import {pageLinks} from '../webScraping/pageLinks';
 
 function PostPreview({data}) {
   const navigation = useNavigation();
   function onPressPost() {
     navigation.push('Post', {data});
   }
-  //   rowid
-  //   sira,
-  //   link,
-  //   mesaj,
-  //   yayinTarihi,
-  //   sonYayinTarihi,
-
-  // const data = {
-
-  //   type: 'Type Deneme',
-  //   position: 'Pozisyon',
-  //   company: {
-  //     name: 'Company Name',
-  //   },
-  //   location: 'Location',
-  // };
 
   return (
     <TouchableOpacity
@@ -37,29 +17,13 @@ function PostPreview({data}) {
       <View style={styles.top}>
         <View style={{flex: 1}}>
           <Text style={styles.title}>{data.mesaj}</Text>
-          {/* <PostType type={data.type} /> */}
         </View>
       </View>
       <View style={styles.info}>
-        {/* <View style={styles.infoItem}>
-          <Icon name="briefcase" color="#333" size={14} />
-          <Text style={styles.infoText} numberOfLines={1}>
-            {data.company.name}
-            company name
-          </Text>
-        </View> */}
-        {/* <View style={styles.infoItem}>
-          <Icon name="map-pin" color="#333" size={14} />
-          <Text style={styles.infoText} numberOfLines={1}>
-            {data.link}
-          </Text>
-        </View> */}
         <View style={styles.infoItem}>
           <Icon name="clock" color="#333" size={14} />
           <Text style={styles.infoText} numberOfLines={1}>
             {data.yayinTarihi}
-            {/* time since */}
-            {/* {timeSince(data.updated_at)} */}
           </Text>
         </View>
 
@@ -67,21 +31,9 @@ function PostPreview({data}) {
           <Icon name="clock" color="#333" size={14} />
           <Text style={styles.infoText} numberOfLines={1}>
             {data.sonYayinTarihi}
-            {/* time since */}
-            {/* {timeSince(data.updated_at)} */}
           </Text>
         </View>
       </View>
-
-      {/* {data.tags.length ? (
-        <Tags
-          tags={data.tags}
-          slug={data.slug}
-          style={styles.tags}
-          tagStyle={styles.tag}
-          tagTextStyle={styles.tagText}
-        />
-      ) : null} */}
     </TouchableOpacity>
   );
 }
@@ -108,14 +60,6 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     color: '#333',
   },
-  image: {
-    width: 40,
-    height: 40,
-    marginLeft: 20,
-    marginTop: 5,
-    backgroundColor: '#f9f9f9',
-    borderRadius: 20,
-  },
   info: {
     flexDirection: 'row',
     marginTop: 14,
@@ -130,26 +74,6 @@ const styles = StyleSheet.create({
     marginLeft: 6,
     fontSize: 13,
     color: '#666',
-  },
-  tags: {
-    flexWrap: 'wrap',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    marginTop: 14,
-  },
-  tag: {
-    backgroundColor: '#f1f1f1',
-    borderColor: '#eee',
-    borderWidth: 1,
-    borderRadius: 3,
-    paddingVertical: 4,
-    paddingHorizontal: 8,
-    marginRight: 8,
-    marginBottom: 10,
-  },
-  tagText: {
-    color: '#666',
-    fontSize: 13,
   },
 });
 
